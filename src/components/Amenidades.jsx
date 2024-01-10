8a081d86f7f0c11495a7f4b536b79a84bc4cc4a9
MiguelKoh <81209063+MiguelKoh@users.noreply.github.com>
2024-01-10 00:00:03 -0600

    seccion amenidades terminado

--- src/components/Amenidades.jsx
+++ src/components/Amenidades.jsx
@@ -2,7 +2,7 @@ import areaDeJuegos from '../assets/images/amenidades/area_de_juegos.png'
 import asadores from '../assets/images/amenidades/asadores.png'
 import playa from '../assets/images/amenidades/club_de_playa.png'
 import corredor from '../assets/images/amenidades/corredor_cultural.png'
-import crecimientoEconomico from '../assets/images/amenidades/crecimiento_economico.png'
+import crecimiento from '../assets/images/amenidades/crecimiento_economico.png'
 import fogata from '../assets/images/amenidades/fogata.png'
 import gym from '../assets/images/amenidades/gym.png'
 import pet from '../assets/images/amenidades/pet_friendly.png'
@@ -18,8 +18,8 @@ function Amenidades() {
       <div className='flex flex-wrap justify-center mb-12'>
       <h3 className="text-3xl">AMENIDADES</h3>
       </div>
-      <div className="containerAmenidades flex flex-wrap">
-        <div className="flex justify-center items-start gap-16 w-full">
+    <div className='w-full flex justify-center'>
+      <div className="containerAmenidades w-5/6 grid xs:grid-cols-3 md:grid-cols-4 lg:grid-cols-7 gap-5 items-center">
           <div><img src={playa}/> <p>Club de playa</p></div>
           <div><img src={cine}/><p>Cine al aire libre</p></div>
           <div><img src={corredor}/><p>Corredor cultural</p></div>
@@ -27,16 +27,15 @@ function Amenidades() {
           <div><img src={fogata}/><p>Fogata</p></div>
           <div><img src={yoga}/><p>Área de yoga y pilates</p></div>
           <div><img src={pet}/><p>Parque pet friendly</p></div>
-        </div>
-        <div className="flex justify-center items-start gap-16 w-full">
           <div><img src={gym}/><p>Gimnasio al aire libre</p></div>
           <div><img src={temazal} /><p>Temazcal</p></div>
           <div><img src={picnicDia} /><p>Picnic de día</p></div>
           <div><img src={areaDeJuegos} /><p>Juegos infantiles</p></div>
           <div><img src={picnicNoche} /><p>Picnic de noche</p></div>
           <div><img src={pista} /><p>Pista multifuncional</p></div>
-        </div>
+          <div><img src={crecimiento} /><p>Crecimiento economico</p></div>
       </div>
+    </div>
     </div>
   );
 }
